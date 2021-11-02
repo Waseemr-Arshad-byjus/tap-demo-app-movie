ac86b555421db37c1aea56cd0b747547515e408f
waseem <waseem.arshad@byjus.com>
2021-11-02 16:16:30 +0530

    update movie function added

--- client/src/Router.js
+++ client/src/Router.js
@@ -3,21 +3,25 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import AddMovie from "./pages/AddMovie";
 import MovieDetails from "./pages/MovieDetails";
+import UpdateMovie from "./pages/UpdateMovie";
 
 const Router = () => {
-    return (
-        <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            <Route exact path="/add-movie">
-                <AddMovie />
-            </Route>
-            <Route exact path="/:movieId">
-                <MovieDetails />
-            </Route>
-        </Switch>
-    )
-}
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/add-movie">
+        <AddMovie />
+      </Route>
+      <Route exact path="/update/:movieId">
+        <UpdateMovie />
+      </Route>
+      <Route exact path="/:movieId">
+        <MovieDetails />
+      </Route>
+    </Switch>
+  );
+};
 
 export default Router;
--- server/routes/movieRoutes.js
+++ server/routes/movieRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const { getAllMovies, getMovie, addMovie } = require("../controllers/movieController");
+const {
+  getAllMovies,
+  getMovie,
+  addMovie,
+  updateMovie,
+} = require("../controllers/movieController");
 
 const router = express.Router();
 
 router
-    .get("/", getAllMovies)
-    .get("/:movieId", getMovie)
-    .post("/", addMovie)
+  .get("/", getAllMovies)
+  .get("/:movieId", getMovie)
+  .post("/", addMovie)
+  .patch("/:id", updateMovie);
 
 module.exports = router;
